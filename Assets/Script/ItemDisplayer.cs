34c1edc2a1dab78cc2fba300d78020e5dc35216e
sundzibo <d.z.sun@lancaster.ac.uk>
2024-03-07 20:07:30 +0000

    Item displayer

---
+++ Assets/Script/ItemDisplayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+public class ItemDisplayer : MonoBehaviour
+{
+    public Button iconPrefab;
+    public GameObject grid;
+
+    [NaughtyAttributes.ReadOnly]
+    public List<Transform> gridLocations;
+
+    int gridIndex = 0;
+
+    private void Awake()
+    {
+        foreach (Transform child in grid.transform) {
+            gridLocations.Add(child);
+        }
+    }
+
+    public void DisplayItemInMenu() {
+        Instantiate(iconPrefab, gridLocations[gridIndex].position, Quaternion.identity, this.transform);
+        UpdateGridIndex();
+    }
+
+    void UpdateGridIndex() {
+        gridIndex = (gridIndex + 1) % gridLocations.Count;
+    }
+}
